73a1f1a5b4088fdb138505bdcb856e186c820800
William Dye <will2dye4@mac.com>
2013-03-02 21:13:11 -0500

    Fixing potential issue with forum names.

--- forums/forms.py
+++ forums/forms.py
@@ -1,11 +1,19 @@
 from django.forms import ModelForm
 from django.forms.fields import CharField
 from django.forms.widgets import Textarea
+from django.template.defaultfilters import slugify
 
 from gtphipsi.forums.models import Forum, Thread, Post
 
 
 class ForumForm(ModelForm):
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if name is not None and slugify(name) in ['add', 'subscriptions', 'edit-post']:
+            self._errors['name'] = self.error_class(['That name is not allowed.'])
+            del self.cleaned_data['name']
+        return self.cleaned_data.get('name') if 'name' in self.cleaned_data else None
 
     class Meta:
         model = Forum
